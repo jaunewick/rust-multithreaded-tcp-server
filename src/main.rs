5d3a1d0854d704659072cfcf0dc568b36089c885
Daniel Giao <daniel.giao@icloud.com>
2024-08-18 21:36:36 -0400

    More robust error handling & bound to CPU cores on system

--- src/main.rs
+++ src/main.rs
@@ -7,27 +7,41 @@ use std::{
 };
 
 use rust_multithreaded_tcp_server::ThreadPool;
+extern crate num_cpus;
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-    let pool = ThreadPool::new(4);
+    let listener = TcpListener::bind("127.0.0.1:7878").expect("Failed to bind to address");
+    let num_threads = num_cpus::get();
+    println!("Number of CPU cores available on your system: {}", num_threads);
+    let pool = ThreadPool::new(num_threads);
 
-    for stream in listener.incoming().take(2) {
-        let stream = stream.unwrap();
-        pool.execute(|| {
-            handle_connection(stream);
-        });
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => pool.execute(|| {
+                handle_connection(stream);
+            }),
+            Err(e) => eprintln!("Failed to establish a connection: {}", e),
+        }
     }
 
-    println!("Shutting down server...");
+    println!("Shutting down...");
 }
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
-    // first unwrap handles Option, and the second handles the Result
-    let request_line = buf_reader.lines().next().unwrap().unwrap();
+    let request_line = match buf_reader.lines().next() {
+        Some(Ok(line)) => line,
+        Some(Err(e)) => {
+            eprintln!("Failed to read request line: {}", e);
+            return;
+        }
+        None => {
+            eprintln!("Connection closed before request line was read.");
+            return;
+        }
+    };
 
-    let (status_line, filename) = match &request_line[..] {
+    let (status_line, filename) = match request_line.as_str() {
         "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "test.html"),
         "GET /sleep HTTP/1.1" => {
             thread::sleep(Duration::from_secs(5));
@@ -36,16 +50,18 @@ fn handle_connection(mut stream: TcpStream) {
         _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
     };
 
-    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "test.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    let contents = match fs::read_to_string(filename) {
+        Ok(contents) => contents,
+        Err(e) => {
+            eprintln!("Failed to read file: {}", e);
+            return;
+        }
     };
 
-    let contents = fs::read_to_string(filename).unwrap();
     let length = contents.len();
-
     let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
-    stream.write_all(response.as_bytes()).unwrap();
+    if let Err(e) = stream.write_all(response.as_bytes()) {
+        eprintln!("Failed to send response: {}", e);
+    }
 }
